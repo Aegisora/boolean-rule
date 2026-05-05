08c173118d410d6b1c8a7125bae4c68fcc72c3a6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-05-05 19:59:03 +0400

    [AEGISORA-7] Covered factory methods.

--- tests/Unit/BooleanRuleTest.php
+++ tests/Unit/BooleanRuleTest.php
@@ -2,8 +2,19 @@
 
 namespace Aegisora\Rules\Tests\Unit;
 
+use Aegisora\RuleContract\RuleInterface;
+use Aegisora\Rules\BooleanRule;
 use PHPUnit\Framework\TestCase;
 
 class BooleanRuleTest extends TestCase
 {
+    public function testCreateTruthy(): void
+    {
+        self::assertInstanceOf(RuleInterface::class, BooleanRule::createTruthy());
+    }
+
+    public function testCreateFalsy(): void
+    {
+        self::assertInstanceOf(RuleInterface::class, BooleanRule::createFalsy());
+    }
 }
